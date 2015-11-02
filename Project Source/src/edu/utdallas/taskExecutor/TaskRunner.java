77c902b2967edd757ad4cd1c8e990a9445b9c177
mdw130530- <wertz.michael@gmail.com>
2015-11-02 14:15:25 -0600

    Unsure if setBlockingQueue will pass in the BlockingTaskQueue class which would have remove() method and this will resolve the issues.

---
+++ Project Source/src/edu/utdallas/taskExecutor/TaskRunner.java
@@ -0,0 +1,37 @@
+package edu.utdallas.taskExecutor;
+
+import java.lang.reflect.Array;
+
+/**
+ * Created by mdw130530- on 11/2/2015.
+ */
+public class TaskRunner implements Runnable{
+    //private BlockingQueue taskQueue = null; //Cannot use BlockingQueue from concurrent
+    private Class<?> taskQueue = null;
+    private Task nextTask = null;
+
+    TaskRunner(){
+
+    }
+
+    TaskRunner(Class<?> blockqueue){
+        taskQueue = blockqueue;
+    }
+
+    public void setBlockingQueue(Class<?> blockqueue){
+        //if (blockqueue instanceof BlockingTaskQueue );
+        taskQueue = blockqueue;
+    }
+
+    public Task take(){
+        return taskQueue.remove();
+        // this.BlockingTaskQueue.remove();
+    }
+
+   public void run(){
+       while(true){
+           nextTask = take();
+           nextTask.execute();
+       }
+    }
+}
